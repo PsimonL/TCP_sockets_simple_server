3433151cea95fa7d59159c59694e3a790a825fef
Szymon Rogowski <szymon0r0official@gmail.com>
2023-03-24 01:32:37 +0100

    implementing client

--- client.go
+++ client.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Client start")
-
+	fmt.Println("Client connected...")
+	// Connect to the server
+	conn, err := net.Dial(serverConfig.Type, serverConfig.Host+":"+serverConfig.Port)
+	if err != nil {
+		panic(err.Error())
+		return
+	}
+	defer conn.Close()
 }
